cd54eefe279db7cb8005e189b5b39fd01f786cf9
Alex_Turpb <turpb41@gmail.com>
2018-03-13 19:33:53 +0200

    L24G initial commit

---
+++ app.rb
@@ -0,0 +1,49 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/visit' do
+	@username = params[:username].capitalize
+	@user_phone = params[:user_phone]
+	@user_date = params[:user_date]
+	@prefered_barber = params[:prefered_barber]
+	@color = params[:color]
+
+	# hash
+	hh = {:username => 'Enter name',
+			:user_phone => 'Enter phone',
+			:user_date => 'Enter date'
+		}
+	
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ""
+		return erb :visit
+	else
+		@title = "Success"
+		@message = "OK! <b><i>#{@username}</b></i></br>We've got your Phone: <b><i>#{@user_phone}</b></i></br><b><i>#{@prefered_barber}</b></i> waiting for you at: <b><i>#{@user_date}</b></i></br><b><i>#{@color}</b></i>paint is available"
+
+		users_log = File.open './public/users.txt', 'a'
+		users_log.write "User: #{@username} Phone: #{@user_phone} Date: #{@user_date} Barber: #{@prefered_barber} Paint: #{@color}\n"
+		users_log.close
+		return erb :message
+	end
+end
+
